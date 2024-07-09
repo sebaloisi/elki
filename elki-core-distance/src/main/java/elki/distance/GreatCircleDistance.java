a5cd70d586c5f661541c0216a0ca4df7f151d384
Sebastian Aloisi <s.aloisi@mailfence.com>
2024-07-09 14:03:46 +0200

    fixed GCD

--- elki-core-distance/src/main/java/elki/distance/GreatCircleDistance.java
+++ elki-core-distance/src/main/java/elki/distance/GreatCircleDistance.java
@@ -21,13 +21,13 @@ package elki.distance;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import elki.data.DoubleVector;
+
 import elki.data.NumberVector;
 import elki.data.type.SimpleTypeInformation;
 import elki.math.linearalgebra.VMath;
 import elki.utilities.optionhandling.Parameterizer;
 
-public class GreatCircleDistance implements NumberVectorDistance<DoubleVector>{
+public class GreatCircleDistance implements NumberVectorDistance<NumberVector>{
     /**
      * Static instance
      */
@@ -44,23 +44,23 @@ public class GreatCircleDistance implements NumberVectorDistance<DoubleVector>{
   }
 
     @Override
-    public double distance(DoubleVector o1, DoubleVector o2) {
+    public double distance(NumberVector o1, NumberVector o2) {
+
+        if (o1.equals(o2)){
+          return 0.0;
+        }
+      
         double[] normalO1 = VMath.normalize(o1.toArray());
         double[] normalO2 = VMath.normalize(o2.toArray());
         double dotProduct = VMath.dot(normalO1, normalO2);
         double result = Math.acos(dotProduct);
+        assert result >= 0;
         return result;
     }
 
     @Override
     public SimpleTypeInformation<? super NumberVector> getInputTypeRestriction() {
         return NumberVector.FIELD;
-    }
-
-    @Override
-    public double distance(NumberVector o1, NumberVector o2) {
-        // TODO Auto-generated method stub
-        return 0;
     }
 
     @Override
